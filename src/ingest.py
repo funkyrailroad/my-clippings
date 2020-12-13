42a68637f33b530b2a89c54cfea35b7508dceb62
Jake Atwell <jatwell@alexinc.com>
2020-12-13 16:53:38 -0500

    Format with black

--- src/ingest.py
+++ src/ingest.py
@@ -1,7 +1,8 @@
 import datetime
 
 import psycopg2
-'''
+
+"""
 Types of clippings:
     - Note
     - Highlight
@@ -51,7 +52,7 @@ Clippings
     - Highlight (required)
     - Note (optional)
 
-'''
+"""
 
 
 class Clipping(object):
@@ -63,8 +64,7 @@ class Clipping(object):
         kind: str,
         location: str,
     ):
-        '''Data structure for the different parts of a clipping
-        '''
+        """Data structure for the different parts of a clipping"""
         self.title = title
         self.content = content
         self.dt = dt
@@ -74,15 +74,15 @@ class Clipping(object):
         self.end_loc = self.get_end_loc()
 
     def get_start_loc(self):
-        if self.kind == 'highlight':
-            return int(self.location.split('-')[0])
-        if self.kind == 'note':
+        if self.kind == "highlight":
+            return int(self.location.split("-")[0])
+        if self.kind == "note":
             return None
 
     def get_end_loc(self):
-        if self.kind == 'highlight':
-            return int(self.location.split('-')[1])
-        if self.kind == 'note':
+        if self.kind == "highlight":
+            return int(self.location.split("-")[1])
+        if self.kind == "note":
             return int(self.location)
 
 
@@ -91,13 +91,13 @@ def process_clippings():
 
 
 def process_clipping(clipping):
-    '''Split a clipping into its title, metadata and content '''
+    """Split a clipping into its title, metadata and content """
 
-    clipping = clipping.split('\n')
+    clipping = clipping.split("\n")
     title = clipping.pop(0)
     metadata = clipping.pop(0)
-    assert clipping.pop(0) == '', 'Unexpected Clipping Format'
-    return title, metadata, '\n'.join(clipping)
+    assert clipping.pop(0) == "", "Unexpected Clipping Format"
+    return title, metadata, "\n".join(clipping)
 
 
 def process_metadata(metadata):
@@ -108,34 +108,34 @@ def process_metadata(metadata):
 
 
 def get_clipping_type(metadata):
-    '''Determine if type is highlight or note'''
+    """Determine if type is highlight or note"""
 
-    type_loc = metadata.split('|')[0]
+    type_loc = metadata.split("|")[0]
     _, _, type, _, _ = type_loc.split()
     return type.lower()
 
 
 def get_clipping_location(metadata):
-    '''Might need to differentiate between location and location range'''
+    """Might need to differentiate between location and location range"""
 
-    type_loc = metadata.split('|')[0]
+    type_loc = metadata.split("|")[0]
     _, _, _, _, loc = type_loc.split()
     return loc
 
 
 def get_date(metadata):
-    '''Parse date from metadata'''
+    """Parse date from metadata"""
 
-    temp = metadata.split('|')[1]
-    date = ' '.join(temp.split()[2:])
+    temp = metadata.split("|")[1]
+    date = " ".join(temp.split()[2:])
     return date
 
 
 def convert_parsed_date_to_datetime(date):
-    _, month_name, day, year, time, semi = date.replace(',', '').split()
+    _, month_name, day, year, time, semi = date.replace(",", "").split()
     month = month_name_to_number(month_name)
-    hr, min, sec = time.split(':')
-    if semi == 'PM':
+    hr, min, sec = time.split(":")
+    if semi == "PM":
         hr = int(hr) + 12
     dt = [year, month, day, hr, min, sec]
     dt = [int(x) for x in dt]
@@ -143,141 +143,148 @@ def convert_parsed_date_to_datetime(date):
 
 
 def month_name_to_number(name):
-    '''Convert month name to month number
+    """Convert month name to month number
     https://www.kite.com/python/answers/how-to-convert-between-month-name-and-month-number-in-python
-    '''
+    """
 
     return datetime.datetime.strptime(name, "%B").month
 
 
-def split_clippings(clippings, sep='==========\n'):
-    '''Chunk full clippings text into a list of individual files
+def split_clippings(clippings, sep="==========\n"):
+    """Chunk full clippings text into a list of individual files
     TODO: might need to consider that text may not fit in memory at once
-    (although that would be a ridiculously huge clippings file)'''
+    (although that would be a ridiculously huge clippings file)"""
 
     clippings_list = clippings.split(sep)
     clippings_list = [c[:-1] for c in clippings_list]
     # final item in split will be empty
     return clippings_list[:-1]
 
+
 def get_db_connection(
-        db: str='myclippings',
-        usr: str="postgres",
-        pw: str="mypassword",
-        host: str='127.0.0.1',
-        port='5432'
-        ):
-    connection = psycopg2.connect(database=db, user=usr, password=pw,
-            host=host, port=port)
+    db: str = "myclippings",
+    usr: str = "postgres",
+    pw: str = "mypassword",
+    host: str = "127.0.0.1",
+    port="5432",
+):
+    connection = psycopg2.connect(
+        database=db, user=usr, password=pw, host=host, port=port
+    )
     return connection
 
+
 def create_highlight_table(connection):
-    '''Create postgres table for highlights.
+    """Create postgres table for highlights.
     Unique entries have a unique set of title, location and time
-    '''
+    """
 
     cursor = connection.cursor()
-    query = \
-    '''CREATE TABLE IF NOT EXISTS highlights (
+    query = """CREATE TABLE IF NOT EXISTS highlights (
     id SERIAL,
     title VARCHAR ( 500 ),
     location VARCHAR( 20 ),
     datetime TIMESTAMPTZ,
     content TEXT,
     PRIMARY KEY (title, location, datetime)
-    );'''
+    );"""
     cursor.execute(query)
     connection.commit()
+
 
 def create_note_table(connection):
-    '''Create postgres table for notes.
+    """Create postgres table for notes.
     Unique entries have a unique set of title, location and time
-    '''
+    """
 
     cursor = connection.cursor()
-    query = \
-    '''CREATE TABLE IF NOT EXISTS notes (
+    query = """CREATE TABLE IF NOT EXISTS notes (
     id SERIAL,
     title VARCHAR ( 500 ),
     location VARCHAR( 20 ),
     datetime TIMESTAMPTZ,
     content TEXT,
     PRIMARY KEY (title, location, datetime)
-    );'''
+    );"""
     cursor.execute(query)
     connection.commit()
+
 
 def add_highlight_to_db(clipping, connection):
-    '''Add highlight to database'''
+    """Add highlight to database"""
 
-    assert clipping.kind == 'highlight', f'Clipping is {clipping.kind}'
+    assert clipping.kind == "highlight", f"Clipping is {clipping.kind}"
     title = clipping.title
     location = clipping.location
     dt = clipping.dt
     content = clipping.content
 
     cursor = connection.cursor()
-    query = f'''INSERT INTO highlights
+    query = f"""INSERT INTO highlights
     (title, location, datetime, content)
     VALUES ( '{title}', '{location}', '{dt}', '{content}' );
-    '''
+    """
     cursor.execute(query)
     connection.commit()
+
 
 def add_note_to_db(clipping, connection):
-    '''Add note to database'''
+    """Add note to database"""
 
-    assert clipping.kind == 'note', f'Clipping is {clipping.kind}'
+    assert clipping.kind == "note", f"Clipping is {clipping.kind}"
     title = clipping.title
     location = clipping.location
     dt = clipping.dt
     content = clipping.content
 
     cursor = connection.cursor()
-    query = f'''INSERT INTO notes
+    query = f"""INSERT INTO notes
     (title, location, datetime, content)
     VALUES ( '{title}', '{location}', '{dt}', '{content}' );
-    '''
+    """
     cursor.execute(query)
     connection.commit()
+
 
 def delete_highlight_from_db(clipping, connection):
-    '''Delete highlight from database'''
+    """Delete highlight from database"""
 
-    assert clipping.kind == 'highlight', f'Clipping is {clipping.kind}'
+    assert clipping.kind == "highlight", f"Clipping is {clipping.kind}"
     title = clipping.title
     location = clipping.location
     dt = clipping.dt
     content = clipping.content
 
     cursor = connection.cursor()
-    query = f'''DELETE FROM highlights
+    query = f"""DELETE FROM highlights
     WHERE
     location = '{location}' AND
     datetime = '{dt}' AND
     content = '{content}';
-    '''
+    """
     cursor.execute(query)
     connection.commit()
+
 
 def delete_note_from_db(clipping, connection):
-    '''Delete note from database'''
+    """Delete note from database"""
 
-    assert clipping.kind == 'note', f'Clipping is {clipping.kind}'
+    assert clipping.kind == "note", f"Clipping is {clipping.kind}"
     title = clipping.title
     location = clipping.location
     dt = clipping.dt
     content = clipping.content
 
     cursor = connection.cursor()
-    query = f'''DELETE FROM notes
+    query = f"""DELETE FROM notes
     WHERE
     location = '{location}' AND
     datetime = '{dt}' AND
     content = '{content}';
-    '''
+    """
     cursor.execute(query)
     connection.commit()
+
 
 if __name__ == "__main__":
     pass
--- src/tests/test_ingest.py
+++ src/tests/test_ingest.py
@@ -8,56 +8,57 @@ from ingest import *
 
 class TestIngest(unittest.TestCase):
     def test_process_clipping(self):
-        '''
+        """
         Process a highlight
         Assumption:  the text of a highlight doesn't have any newlines in it,
                 but notes might
-        '''
-        clipping = \
-'''Pro Git (Scott Chacon;Ben Straub)
+        """
+        clipping = """Pro Git (Scott Chacon;Ben Straub)
 - Your Highlight Location 2868-2871 | Added on Saturday, April 18, 2020 11:21:19 AM
 
-comparing the content of the newly-fetched featureA branch with her local copy of the same branch: $ git log featureA..origin/featureA'''
+comparing the content of the newly-fetched featureA branch with her local copy of the same branch: $ git log featureA..origin/featureA"""
         title, metadata, content = process_clipping(clipping)
 
-        assert title == 'Pro Git (Scott Chacon;Ben Straub)', title
-        assert metadata == '- Your Highlight Location 2868-2871 | Added on Saturday, April 18, 2020 11:21:19 AM', metadata
-        assert content == 'comparing the content of the newly-fetched featureA branch with her local copy of the same branch: $ git log featureA..origin/featureA', content
+        assert title == "Pro Git (Scott Chacon;Ben Straub)", title
+        assert (
+            metadata
+            == "- Your Highlight Location 2868-2871 | Added on Saturday, April 18, 2020 11:21:19 AM"
+        ), metadata
+        assert (
+            content
+            == "comparing the content of the newly-fetched featureA branch with her local copy of the same branch: $ git log featureA..origin/featureA"
+        ), content
 
     def test_get_clipping_type(self):
-        metadata = '- Your Highlight Location 2868-2871 | Added on Saturday, April 18, 2020 11:21:19 AM'
+        metadata = "- Your Highlight Location 2868-2871 | Added on Saturday, April 18, 2020 11:21:19 AM"
         type = get_clipping_type(metadata)
-        assert type == 'highlight', type
+        assert type == "highlight", type
 
     def test_get_clipping_location(self):
-        metadata = '- Your Highlight Location 2868-2871 | Added on Saturday, April 18, 2020 11:21:19 AM'
+        metadata = "- Your Highlight Location 2868-2871 | Added on Saturday, April 18, 2020 11:21:19 AM"
         loc = get_clipping_location(metadata)
-        assert loc == '2868-2871', loc
+        assert loc == "2868-2871", loc
 
     def test_get_date(self):
-        metadata = '- Your Highlight Location 2868-2871 | Added on Saturday, April 18, 2020 11:21:19 AM'
+        metadata = "- Your Highlight Location 2868-2871 | Added on Saturday, April 18, 2020 11:21:19 AM"
         date = get_date(metadata)
-        assert date == 'Saturday, April 18, 2020 11:21:19 AM'
+        assert date == "Saturday, April 18, 2020 11:21:19 AM"
 
     def test_convert_parsed_date_to_datetime(self):
-        date = 'Saturday, April 18, 2020 11:21:19 PM'
+        date = "Saturday, April 18, 2020 11:21:19 PM"
         dt = convert_parsed_date_to_datetime(date)
-        assert dt == datetime.datetime(2020,
-                                       4,
-                                       18,
-                                       23,
-                                       21,
-                                       19,
-                                       tzinfo=datetime.timezone.utc)
+        assert dt == datetime.datetime(
+            2020, 4, 18, 23, 21, 19, tzinfo=datetime.timezone.utc
+        )
 
     def test_month_name_to_number(self):
-        name = 'April'
+        name = "April"
         number = month_name_to_number(name)
         assert number == 4
 
     def test_split_clippings(self):
 
-        clippings = '''The Compound Effect (Darren Hardy)
+        clippings = """The Compound Effect (Darren Hardy)
 - Your Highlight Location 626-626 | Added on Friday, December 11, 2020 1:42:54 PM
 
 Become very conscious of every choice you make today so you can begin to make smarter choices moving forward.
@@ -72,22 +73,22 @@ The Compound Effect (Darren Hardy)
 
 Do you know how the casinos make so much money in Vegas? Because they track every table, every winner, every hour. Why do Olympic trainers get paid top dollar? Because they track every workout, every calorie, and every micronutrient for their athletes. All winners are trackers. Right now I want you to track your life with the same intention: to bring your goals within sight.
 ==========
-'''
+"""
 
-        clipping_1 = '''The Compound Effect (Darren Hardy)
+        clipping_1 = """The Compound Effect (Darren Hardy)
 - Your Highlight Location 626-626 | Added on Friday, December 11, 2020 1:42:54 PM
 
-Become very conscious of every choice you make today so you can begin to make smarter choices moving forward.'''
+Become very conscious of every choice you make today so you can begin to make smarter choices moving forward."""
 
-        clipping_2 = '''The Compound Effect (Darren Hardy)
+        clipping_2 = """The Compound Effect (Darren Hardy)
 - Your Highlight Location 636-637 | Added on Friday, December 11, 2020 1:45:14 PM
 
-The biggest difference between successful people and unsuccessful people is that successful people are willing to do what unsuccessful people are not.'''
+The biggest difference between successful people and unsuccessful people is that successful people are willing to do what unsuccessful people are not."""
 
-        clipping_3 = '''The Compound Effect (Darren Hardy)
+        clipping_3 = """The Compound Effect (Darren Hardy)
 - Your Highlight Location 666-668 | Added on Friday, December 11, 2020 1:49:33 PM
 
-Do you know how the casinos make so much money in Vegas? Because they track every table, every winner, every hour. Why do Olympic trainers get paid top dollar? Because they track every workout, every calorie, and every micronutrient for their athletes. All winners are trackers. Right now I want you to track your life with the same intention: to bring your goals within sight.'''
+Do you know how the casinos make so much money in Vegas? Because they track every table, every winner, every hour. Why do Olympic trainers get paid top dollar? Because they track every workout, every calorie, and every micronutrient for their athletes. All winners are trackers. Right now I want you to track your life with the same intention: to bring your goals within sight."""
 
         answer = [clipping_1, clipping_2, clipping_3]
 
@@ -98,10 +99,10 @@ Do you know how the casinos make so much money in Vegas? Because they track ever
 
 class TestClipping(unittest.TestCase):
     def setUp(self):
-        self.raw_highlight = '''The Compound Effect (Darren Hardy)
+        self.raw_highlight = """The Compound Effect (Darren Hardy)
 - Your Highlight Location 666-668 | Added on Friday, December 11, 2020 1:49:33 PM
 
-Do you know how the casinos make so much money in Vegas? Because they track every table, every winner, every hour. Why do Olympic trainers get paid top dollar? Because they track every workout, every calorie, and every micronutrient for their athletes. All winners are trackers. Right now I want you to track your life with the same intention: to bring your goals within sight.'''
+Do you know how the casinos make so much money in Vegas? Because they track every table, every winner, every hour. Why do Olympic trainers get paid top dollar? Because they track every workout, every calorie, and every micronutrient for their athletes. All winners are trackers. Right now I want you to track your life with the same intention: to bring your goals within sight."""
         title, metadata, content = process_clipping(self.raw_highlight)
         date = get_date(metadata)
         dt = convert_parsed_date_to_datetime(date)
@@ -109,10 +110,10 @@ Do you know how the casinos make so much money in Vegas? Because they track ever
         kind = get_clipping_type(metadata)
         self.highlight = Clipping(title, content, dt, kind, location)
 
-        self.raw_note = '''The Compound Effect (Darren Hardy)
+        self.raw_note = """The Compound Effect (Darren Hardy)
 - Your Note Location 548 | Added on Friday, December 11, 2020 1:24:32 PM
 
-amazingly thoughtful and mutually beneficial gift idea for a loved one'''
+amazingly thoughtful and mutually beneficial gift idea for a loved one"""
         title, metadata, content = process_clipping(self.raw_note)
         date = get_date(metadata)
         dt = convert_parsed_date_to_datetime(date)
@@ -121,14 +122,18 @@ amazingly thoughtful and mutually beneficial gift idea for a loved one'''
         self.note = Clipping(title, content, dt, kind, location)
 
     def test_init(self):
-        '''Test with no note, only highlight'''
+        """Test with no note, only highlight"""
 
-        assert self.highlight.title == 'The Compound Effect (Darren Hardy)'
-        assert self.highlight.content == 'Do you know how the casinos make so much money in Vegas? Because they track every table, every winner, every hour. Why do Olympic trainers get paid top dollar? Because they track every workout, every calorie, and every micronutrient for their athletes. All winners are trackers. Right now I want you to track your life with the same intention: to bring your goals within sight.'
+        assert self.highlight.title == "The Compound Effect (Darren Hardy)"
+        assert (
+            self.highlight.content
+            == "Do you know how the casinos make so much money in Vegas? Because they track every table, every winner, every hour. Why do Olympic trainers get paid top dollar? Because they track every workout, every calorie, and every micronutrient for their athletes. All winners are trackers. Right now I want you to track your life with the same intention: to bring your goals within sight."
+        )
         assert self.highlight.dt == datetime.datetime(
-            2020, 12, 11, 13, 49, 33, tzinfo=datetime.timezone.utc)
-        assert self.highlight.kind == 'highlight'
-        assert self.highlight.location == '666-668'
+            2020, 12, 11, 13, 49, 33, tzinfo=datetime.timezone.utc
+        )
+        assert self.highlight.kind == "highlight"
+        assert self.highlight.location == "666-668"
 
     def test_get_start_loc(self):
         start_loc = self.highlight.get_start_loc()
@@ -148,11 +153,11 @@ amazingly thoughtful and mutually beneficial gift idea for a loved one'''
 class TestPostgres(unittest.TestCase):
     def setUp(self):
         # grab these from env vars
-        self.db = 'test_myclippings'
+        self.db = "test_myclippings"
         self.usr = "postgres"
         self.pw = "mypassword"
-        self.host = '127.0.0.1'
-        self.port = '5432'
+        self.host = "127.0.0.1"
+        self.port = "5432"
 
         self.create_test_db()
         self.connection = self.get_test_db_connection()
@@ -161,50 +166,48 @@ class TestPostgres(unittest.TestCase):
         create_note_table(self.connection)
 
     def get_test_db_connection(self):
-        '''Use this connection for everything except creating databases'''
+        """Use this connection for everything except creating databases"""
 
-        connection = psycopg2.connect(database=self.db,
-                                      user=self.usr,
-                                      password=self.pw,
-                                      host=self.host,
-                                      port=self.port)
+        connection = psycopg2.connect(
+            database=self.db,
+            user=self.usr,
+            password=self.pw,
+            host=self.host,
+            port=self.port,
+        )
         return connection
 
     def create_test_db(self):
-        '''Create the test database
-        https://pythontic.com/database/postgresql/create%20database'''
+        """Create the test database
+        https://pythontic.com/database/postgresql/create%20database"""
 
-        connection = psycopg2.connect(user=self.usr,
-                                      password=self.pw,
-                                      host=self.host,
-                                      port=self.port)
-        connection.set_isolation_level(
-            psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+        connection = psycopg2.connect(
+            user=self.usr, password=self.pw, host=self.host, port=self.port
+        )
+        connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
         cursor = connection.cursor()
-        query = '''CREATE DATABASE "test_myclippings";'''
+        query = """CREATE DATABASE "test_myclippings";"""
         cursor.execute(query)
         connection.commit()
 
     def destroy_test_db(self):
-        '''Destroy the test database'''
+        """Destroy the test database"""
 
-        connection = psycopg2.connect(user=self.usr,
-                                      password=self.pw,
-                                      host=self.host,
-                                      port=self.port)
-        connection.set_isolation_level(
-            psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+        connection = psycopg2.connect(
+            user=self.usr, password=self.pw, host=self.host, port=self.port
+        )
+        connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
         cursor = connection.cursor()
-        query = '''DROP DATABASE "test_myclippings";'''
+        query = """DROP DATABASE "test_myclippings";"""
         cursor.execute(query)
         connection.commit()
 
     def test_highlights(self):
-        '''Test for adding and deleting highlights from the database'''
-        raw_highlight = '''The Compound Effect (Darren Hardy)
+        """Test for adding and deleting highlights from the database"""
+        raw_highlight = """The Compound Effect (Darren Hardy)
 - Your Highlight Location 666-668 | Added on Friday, December 11, 2020 1:49:33 PM
 
-Do you know how the casinos make so much money in Vegas? Because they track every table, every winner, every hour. Why do Olympic trainers get paid top dollar? Because they track every workout, every calorie, and every micronutrient for their athletes. All winners are trackers. Right now I want you to track your life with the same intention: to bring your goals within sight.'''
+Do you know how the casinos make so much money in Vegas? Because they track every table, every winner, every hour. Why do Olympic trainers get paid top dollar? Because they track every workout, every calorie, and every micronutrient for their athletes. All winners are trackers. Right now I want you to track your life with the same intention: to bring your goals within sight."""
         title, metadata, content = process_clipping(raw_highlight)
         date = get_date(metadata)
         dt = convert_parsed_date_to_datetime(date)
@@ -216,11 +219,11 @@ Do you know how the casinos make so much money in Vegas? Because they track ever
         delete_highlight_from_db(highlight, self.connection)
 
     def test_notes(self):
-        '''Test for adding and deleting notes from the database'''
-        raw_note = '''The Compound Effect (Darren Hardy)
+        """Test for adding and deleting notes from the database"""
+        raw_note = """The Compound Effect (Darren Hardy)
 - Your Note Location 548 | Added on Friday, December 11, 2020 1:24:32 PM
 
-amazingly thoughtful and mutually beneficial gift idea for a loved one'''
+amazingly thoughtful and mutually beneficial gift idea for a loved one"""
         title, metadata, content = process_clipping(raw_note)
         date = get_date(metadata)
         dt = convert_parsed_date_to_datetime(date)
